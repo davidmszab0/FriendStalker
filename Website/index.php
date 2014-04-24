5edd44cf8f72926a2b5da3e4776d22ab9f71249d
David M. Szabo <szdave@msn.com>
2014-04-24 18:50:14 +0200

    website 2 php files

---
+++ Website/index.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "hello boy!";
+
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>City of Assassins Application</title>	
+	</head>
+	<body>
+		<h1>This is the Game you ever dreamed of!</h1>
+		<p>and this is just the beginning...</p>
+	</body>
+</html>
