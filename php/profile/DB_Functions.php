b1f516c7b23a5c92c2d5ac142ab933e728eb5638
GiantSusus <szdave@msn.com>
2014-05-08 09:50:00 +0200

    new profile folder

---
+++ php/profile/DB_Functions.php
@@ -0,0 +1,86 @@
+<?php
+ 
+class DB_Functions {
+ 
+    private $db;
+ 
+    //put your code here
+    // constructor
+    function __construct() {
+        require_once 'DB_Connect.php';
+        // connecting to database
+        $this->db = new DB_Connect();
+        $this->db->connect();
+    }
+ 
+    // destructor
+    function __destruct() {
+         
+    }
+
+    function getUserKills($uuid) {
+        $result = mysql_query("SELECT noKills FROM Statistics INNER JOIN Account ON 
+            Account.account_id = Statistics.account_id WHERE unique_id = '$uuid'");
+        $no_of_rows = mysql_num_rows($result);
+
+        if ($no_of_rows > 0) {
+            $result = mysql_fetch_array($result);
+            return $result;    
+        } else {
+            return false;
+        }
+    }
+
+    function getUserDeaths($uuid) {
+        $result = mysql_query("SELECT noDeaths FROM Statistics INNER JOIN Account ON 
+            Account.account_id = Statistics.account_id WHERE unique_id = '$uuid'");
+        $no_of_rows = mysql_num_rows($result);
+
+        if ($no_of_rows > 0) {
+            $result = mysql_fetch_array($result);
+            return $result;    
+        } else {
+            return false;
+        }
+    }
+
+    function userExists($uuid) {
+        $result = mysql_query("SELECT * FROM Account WHERE unique_id = '$uuid'");
+        $no_of_rows = mysql_num_rows($result);
+        if ($no_of_rows > 0) {
+            // user existed
+            return true;
+        } else {
+            // user not existed
+            return false;
+        }
+    }
+
+    function getUserWeapon($uuid) {
+        $result = mysql_query("SELECT * FROM Weapon 
+            INNER JOIN  Weapon ON Weapon.weaponID = PlayerWeapon.weaponID 
+            INNER JOIN Account ON Account.account_id = PlayerWeapon.account_id
+            WHERE Account.unique_id = '$uuid'");
+
+        if ($no_of_rows > 0) {
+            $result = mysql_fetch_array($result);
+            return $result;    
+        } else {
+            return false;
+        }
+    }
+    function getUserArmour($uuid) {
+        $result = mysql_query("SELECT * FROM Armour 
+            INNER JOIN  Armour ON Armour.armour_id = PlayerArmour.armour_id 
+            INNER JOIN Account ON Account.account_id = PlayerArmour.account_id
+            WHERE Account.unique_id = '$uuid'");
+
+        if ($no_of_rows > 0) {
+            $result = mysql_fetch_array($result);
+            return $result;    
+        } else {
+            return false;
+        }
+    }
+ 
+?>
