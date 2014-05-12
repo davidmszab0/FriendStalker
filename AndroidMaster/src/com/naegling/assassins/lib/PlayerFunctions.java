c8adb71e29935d3c46b70bb60b074e0d2742a3f9
GiantSusus <szdave@msn.com>
2014-05-12 11:02:03 +0200

    changes after the Get Target function is done

--- AndroidMaster/src/com/naegling/assassins/lib/PlayerFunctions.java
+++ AndroidMaster/src/com/naegling/assassins/lib/PlayerFunctions.java
@@ -25,9 +25,13 @@ public class PlayerFunctions {
     private static String getTargetLocationTag = "get_target_location";
     private static String setOnlineStatusTag = "set_online_status";
     private static String getAllTag = "get_all";
+    private static String getAlluid2 = "get_all_uid";
+    private static String updateTargetTag = "update_target";
 
 
-    public PlayerFunctions() {jsonParser = new JSONParser();}
+    public PlayerFunctions() {
+    	jsonParser = new JSONParser(); 
+    }
 
     public JSONObject updatePlayerLocation(Context context, Location location, String status){
         DatabaseHandler dbh = new DatabaseHandler(context);
@@ -43,10 +47,12 @@ public class PlayerFunctions {
         params.add(new BasicNameValuePair("status", status));
 
         return jsonParser.getJSONFromUrl(playerURL, params);
-
     }
 
     public JSONObject getTargetLocation(String target){
+    	//DatabaseHandler dbh = new DatabaseHandler(context);
+        //HashMap hm = dbh.getUserDetails();
+        //String uid = (String) hm.get("uid");
 
         List<NameValuePair> params = new ArrayList<NameValuePair>();
         params.add(new BasicNameValuePair("tag", getTargetLocationTag));
@@ -69,11 +75,33 @@ public class PlayerFunctions {
     }
 
     public JSONArray getAllOnline(){
+    	
         List<NameValuePair> params = new ArrayList<NameValuePair>();
         params.add(new BasicNameValuePair("tag", getAllTag));
 
         return jsonParser.getJSONArrayFromUrl(playerURL, params);
     }
+    
+    public JSONObject updateTarget(Context context, String target) {
+    	DatabaseHandler dbh = new DatabaseHandler(context);
+    	HashMap hm = dbh.getUserDetails();
+        String uid = (String) hm.get("uid");
+    	
+    	List<NameValuePair> params = new ArrayList<NameValuePair>();
+    	params.add(new BasicNameValuePair("tag", updateTargetTag));
+    	params.add(new BasicNameValuePair("uuid", uid));
+    	params.add(new BasicNameValuePair("targetUuid", target));
+    	
+    	return jsonParser.getJSONFromUrl(playerURL, params);
+    	
+    }
+    
+    public JSONArray getAlluid(){
+        
+    	List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("tag", getAlluid2));
 
+        return jsonParser.getJSONArrayFromUrl(playerURL, params);
+    }
 }
 
